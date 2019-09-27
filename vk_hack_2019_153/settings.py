e0793142ec30cb4be4f4e2d2e90cfcf2810d317d
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2019-09-27 22:26:54 +0300

    local settigs

--- vk_hack_2019_153/settings.py
+++ vk_hack_2019_153/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '0jhfxl&x7^rg*0n$s2-=188dxrvyc+i!b*dbxtg(mrx$1b7wze'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '95.213.37.131']
 
 # Application definition
 
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')

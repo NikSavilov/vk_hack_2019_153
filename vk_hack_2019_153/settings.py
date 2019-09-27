344244f69e3ed6acb29947d90a1737685b6a604f
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2019-09-27 22:31:59 +0300

    settigs fix

--- vk_hack_2019_153/settings.py
+++ vk_hack_2019_153/settings.py
@@ -121,3 +121,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+try:
+	from vk_hack_2019_153.local_settings import *
+except ImportError:
+	pass
